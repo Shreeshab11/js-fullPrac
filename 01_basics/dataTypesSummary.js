3494d5fcc3b67fb740fc209cb1e93d9dcaaafa83
Shreeshab11 <128978552+Shreeshab11@users.noreply.github.com>
2025-03-05 04:53:58 +0000

    dataypes summary

---
+++ 01_basics/dataTypesSummary.js
@@ -0,0 +1,21 @@
+//Primitives
+/*
+Boolean
+Number
+
+*/ 
+
+const nigNum = 123456789999999999999n;
+console.log(typeof nigNum);
+const arr = ["Hello",23,"Java"]
+
+console.log(typeof arr);
+
+const func =  function(){
+    console.log("Inside function");
+    
+}
+
+func();
+console.log(typeof func);
+
