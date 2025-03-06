b16df553c57449a1c1f2e36921a6abf2f2cb88d4
Shreeshab11 <128978552+Shreeshab11@users.noreply.github.com>
2025-03-06 11:41:23 +0000

    arrows

---
+++ 03_basics/03_arrows.js
@@ -0,0 +1,16 @@
+// declaration of arrow
+
+// ()=>{} 
+    const arrow = (n1,n2)=>{
+
+        return n1+n2;
+    }
+
+    const arrow1 = (n1,n2)=> (n1+n2); //another way of decalring arrow function implicit arrow func
+
+    //Immediately Invoked Function Expression
+
+   (function (){
+        console.log("DB CONNECTED");
+        
+    })()
